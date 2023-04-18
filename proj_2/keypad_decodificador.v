8341e133be636eff3f0f3e76a12479671a50c055
quote023 <acs2@ecomp.poli.br>
2023-04-18 01:11:33 -0300

    refactor

--- proj_2/keypad_decodificador.v
+++ proj_2/keypad_decodificador.v
@@ -15,10 +15,9 @@ reg [1:0] state = 0;
 // A cada ciclo de (+/- 10/ms) troca qual coluna fica com nível lógico 0
 // Essa troca rápida permite identificar qual coluna tá sendo pressionada
 always @(posedge clk) begin   
-	if(counter < cycle) begin
+	if(counter < cycle) 
 		if(key == TECLAS.T_NULL) counter <= 0; // Se a tecla apertada for solta => zera a contagem
 		else counter <= counter + 1;
-	end
 	else begin 
 		counter <= 0;
 		state <= state + 1;
@@ -32,31 +31,28 @@ always @(posedge clk) begin
 end
 //--------------- KEYPAD- VERIFICA LINHAS --------------------
 always @ (posedge clk) begin
-	if(~&linhas) begin 
-		if (~colunas[0]) begin
+	if(~&linhas)
+		if (~colunas[0]) 
 			if 		(~linhas[0]) 	number <= TECLAS.T_1;
 			else if (~linhas[1]) 	number <= TECLAS.T_4;
 			else if (~linhas[2]) 	number <= TECLAS.T_7;
 			else if (~linhas[3]) 	number <= TECLAS.T_ASTE;
-		end else if (~colunas[1]) begin
+		else if (~colunas[1]) 
 			if 		(~linhas[0]) 	number <= TECLAS.T_2;
 			else if (~linhas[1]) 	number <= TECLAS.T_5;
 			else if (~linhas[2]) 	number <= TECLAS.T_8;
 			else if (~linhas[3]) 	number <= TECLAS.T_0;
-		end else if (~colunas[2]) begin
+		else if (~colunas[2]) 
 			if 		(~linhas[0]) 	number <= TECLAS.T_3;
 			else if (~linhas[1]) 	number <= TECLAS.T_6;
 			else if (~linhas[2]) 	number <= TECLAS.T_9;
 			else if (~linhas[3]) 	number <= TECLAS.T_HASH;
-		end else if (~colunas[3]) begin
+		else if (~colunas[3]) 
 			if 		(~linhas[0])	number <= TECLAS.T_A;
 			else if (~linhas[1]) 	number <= TECLAS.T_B;
 			else if (~linhas[2]) 	number <= TECLAS.T_C;
 			else if (~linhas[3]) 	number <= TECLAS.T_D;
-		end
-	end
-	else begin 
+	else 
 		key <= TECLAS.T_NULL;
-	end
 end
 endmodule 
